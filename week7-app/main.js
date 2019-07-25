11e8cd9786db44fe4dcf38cef3bbb13810fb4791
theClubhou.se <marsackg7@gmail.com>
2019-07-25 14:34:23 -0400

    server madness

--- week7-app/main.js
+++ week7-app/main.js
@@ -1,42 +1,72 @@
-"use strict"
-​
+// var {server} = require('./server');
+
 var menu = {fries: 3, burger: 2};
+
 var input = document.querySelector('input');
 var button = document.querySelector('button');
+// var inventoryBtn = document.querySelector('btn');
+
+// inventoryBtn.addEventListener('click', function(){
+//     for(var item in menu){
+//         document.open();
+//         document.write("<h1>" + item + menu[item] + "</h1>");
+//         document.close();
+//     }
+//     console.log(menu);
+    
+// });
+
+
+
 button.addEventListener('click', function(){
     var customerOrder = input.value;
     customerOrder = customerOrder.split(',');
-    
-    // console.log(customerOrder);
-    
+    console.log(customerOrder);
+
     for(var i = 0; i < customerOrder.length; i++){
- 
-        var customerItem = customerOrder[i].split(':');
-​
-​
+         var customerItem = customerOrder[i].split(':');
+
         for(var item in menu) {
-               customerItem = menu[item]; 
-               console.log(customerItem[0], item);
-              if(item === customerItem[0]){
-                  console.log("Thanks for your order!")
-              } else {
-                //   console.log(item);
-                //   console.log(menu[item]);
-                  console.log("Not on menu");
-              }
+   
+            if(item === customerItem[0]){
+
+                if (menu[item] >= customerItem[1]){
+                    console.log("You ordered " + customerItem[1] + " "+ customerItem[0]);
+                    menu[item] -= customerItem[1];
+                    console.log("We now have " + menu[item]);
+                } 
+                else {
+                    console.log("Sorry we only have " + menu[item]);
+            }       
+        }        
+            else {
+                console.log("Not on menu");
             }
-        
+        }
+    } 
+    loadDoc();
+})
+
+function loadDoc(){
+
+    var funnel = new XMLHttpRequest();
+     
+    funnel.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+             console.log("the ready state is " + funnel.readyState);
+                 console.log(this.responseText);  
+                 console.log(responseText);     
+        } else {
+            console.log(this.readyState, this.status);
     }
+}
+funnel.open("GET","http://localhost:8080/hi/hello");
+funnel.send();
 
-       
-    
-    
-        
-               
-        })  
- 
 
-        inventoryBtn.addEventListener('click', function(){
-            return(menu[item]);
-            
-        });
+
+}
+
+
+
+// server();
--- week7-app/server.js
+++ week7-app/server.js
@@ -1,8 +1,29 @@
 var http = require('http');
+var url = require('url');
 
-var PORT = 5500;
-var server = http.createServer((request, response) => {
 
+
+
+var server = http.createServer(function (request, response) {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+       
+        // var filePath = '.' + parseUrl.pathname;
+        var parseUrl = url.parse(request.url, true);
+        
+       
+
+        
+                
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Ta Da we're done! ");
+        response.end();
+        console.log(parseUrl.pathname);
+        
+    }); 
+
+            
+
+server.listen(8080, function () {
+ console.log('server is listening');
 });
 
-server.listen(PORT, () => console.log('server is listening on port {5500}'));
